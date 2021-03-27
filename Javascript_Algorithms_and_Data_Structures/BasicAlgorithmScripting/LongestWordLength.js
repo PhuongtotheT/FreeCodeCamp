eba2b0e06e085f7d9ac5b0efa2a2aabfd8eb7211
Phuong Tran <PhuongtotheT@gmail.com>
2021-03-26 22:56:26 -0700

    simple function that returns the length of the longest word in a sentence

---
+++ Javascript_Algorithms_and_Data_Structures/BasicAlgorithmScripting/LongestWordLength.js
@@ -0,0 +1,29 @@
+/*
+
+Return the length of the longest word in the provided sentence.
+
+Your response should be a number.
+
+findLongestWordLength("The quick brown fox jumped over the lazy dog") should return a number.
+
+findLongestWordLength("The quick brown fox jumped over the lazy dog") should return 6.
+
+findLongestWordLength("May the force be with you") should return 5.
+
+findLongestWordLength("Google do a barrel roll") should return 6.
+
+findLongestWordLength("What is the average airspeed velocity of an unladen swallow") should return 8.
+
+findLongestWordLength("What if we try a super-long word such as otorhinolaryngology") should return 19.
+*/
+
+function findLongestWordLength(str) {
+  let longest = 0;
+  let splitStr = str.split(' ');
+  splitStr.forEach((word) => {
+    longest = word.length > longest ? word.length : longest;
+  });
+  return longest;
+}
+
+findLongestWordLength("The quick brown fox jumped over the lazy dog");
